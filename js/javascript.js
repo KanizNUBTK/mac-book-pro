368e81617da5e7fade7c853bae286d5489333983
KanizNUBTK <kaniz.m.mim@gmail.com>
2021-08-21 17:24:21 +0600

    promo code commited

--- js/javascript.js
+++ js/javascript.js
@@ -1,32 +1,65 @@
-console.log('okey');
-// get product price form input value
-function changeInputValue(product, price) {
-    const memoryCostInput = document.getElementById(product + '-total');
-    memoryCostInput.innerText = price;
-    const totalPriceInput = document.getElementById('bestPrice');
-    const totalPrice = parseInt(totalPriceInput.innerText);
-    if (price == 0) {
+// console.log('okey');
+const memoryCost = document.getElementById('memory-total');
+const storageCost = document.getElementById('storage-total');
+const deliverCost = document.getElementById('delivery-total');
+const bestPrice = document.getElementById('bestPrice');
+const price = document.getElementById('totalPrice');
 
-    }
-}
 document.getElementById('memory-type-8GB').addEventListener('click', function () {
-    changeInputValue('memory', 0);
+    memoryCost.innerText = "0";
+    updatePrice();
 })
 document.getElementById('memory-type-16Gb').addEventListener('click', function () {
-    changeInputValue('memory', 180);
+    memoryCost.innerText = "180";
+    updatePrice();
 })
 document.getElementById('storage-SSD-256').addEventListener('click', function () {
-    changeInputValue('storage', 0);
+    storageCost.innerText = "0";
+    updatePrice();
 })
 document.getElementById('storage-SSD-512').addEventListener('click', function () {
-    changeInputValue('storage', 100);
+    storageCost.innerText = "100";
+    updatePrice();
 })
 document.getElementById('storage-SSD-1T').addEventListener('click', function () {
-    changeInputValue('storage', 180);
+    storageCost.innerText = "180";
+    updatePrice();
 })
 document.getElementById('deliveryFree').addEventListener('click', function () {
-    changeInputValue('delivery', 0);
+    deliverCost.innerText = "0";
+    updatePrice();
 })
 document.getElementById('deliveryCost').addEventListener('click', function () {
-    changeInputValue('delivery', 20);
+    deliverCost.innerText = "20";
+    updatePrice();
+})
+
+// updated price
+bestPrice.innerText = "1299";
+function updatePrice() {
+    const productPrice = Number(bestPrice.innerText);
+    const memoryPrice = Number(memoryCost.innerText);
+    const storagePrice = Number(storageCost.innerText);
+    const delivaryPrice = Number(deliverCost.innerText);
+    const totalPrice = productPrice + memoryPrice + storagePrice + delivaryPrice;
+    price.innerText = totalPrice;
+    promoCode();
+}
+// promo code
+const promoCodeInput = document.getElementById('promo-input');
+const finalPrice = document.getElementById('finalPrice');
+function promoCode() {
+    if (promoCodeInput.value.toLowerCase() == 'stevekaku') {
+        const promoCodeNumber = parseFloat(price.innerText * 20) / 100;
+        // console.log(promoCodeNumber);
+        finalPrice.innerText = parseFloat(price.innerText) - promoCodeNumber;
+        // console.log(finalPrice);
+    } else {
+        finalPrice.innerText = price.innerText;
+        // console.log(finalPrice);
+    }
+    promoCodeInput.value = '';
+}
+document.getElementById('promo-btn').addEventListener('click', function () {
+    promoCode();
 })
